6cc040ab0b89e7297b3acedf4d9304552c24cc30
Shamindi Hettisinghe <99594744+Shami1211@users.noreply.github.com>
2025-03-15 16:40:15 +0530

    change

--- backend/app.py
+++ backend/app.py
@@ -13,10 +13,10 @@ app = Flask(__name__)
 #from routes.KnestheticLearning.SmileFileRoute import SmileFileRoute
 
 #ReadWrite Learning
-from routes.ReadWriteLearning.letter_routes import letter_routes
+#from routes.ReadWriteLearning.letter_routes import letter_routes
 
 #Visual Learning
-from routes.VisualLearning.color_matching_routes import color_matching_routes
+#from routes.VisualLearning.color_matching_routes import color_matching_routes
 
 
 # Apply CORS properly AFTER initializing app
@@ -40,20 +40,23 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
 #app.register_blueprint(LetterRoutes) 
 #app.register_blueprint(SmileFileRoute)
 
-app.register_blueprint(letter_routes, url_prefix='/read_write_learning')
+#app.register_blueprint(letter_routes, url_prefix='/read_write_learning')
 
-app.register_blueprint(color_matching_routes, url_prefix='/visual_learning')
+#app.register_blueprint(color_matching_routes, url_prefix='/visual_learning')
 
 
 # Import routes AFTER initializing Flask app
 from routes.AuditoryLearning.AudioBook.record_routes import record_routes
 from routes.AuditoryLearning.Guiders.lesson_routes import lesson_routes
 from routes.AuditoryLearning.AudioGame.audiogame_routes import audiogame_routes
+from routes.AuditoryLearning.AudioBook.save_progress import save_progress
 from routes.auth.auth_routes import auth_routes
 
 # Register Blueprints for Auditory Learning
+CORS(record_routes, origins="http://localhost:3000")
 app.register_blueprint(record_routes)
 app.register_blueprint(lesson_routes, url_prefix="/api")
+app.register_blueprint(save_progress, url_prefix="/api")
 app.register_blueprint(audiogame_routes, url_prefix="/api")
 app.register_blueprint(auth_routes, url_prefix='/auth')
 
